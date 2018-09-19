09a0af03ec05afd3e328b1fab32a2f74642b85f9
Dmitrij Koniajev <dimchansky@gmail.com>
2018-09-19 10:14:05 +0300

    fixed build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,14 +9,15 @@ if [ -z "$CI_PIPELINE_ID" ]
     exit 1
 fi
 
+if [ -z "$PACKAGE_NAME" ]
+  then
+    echo "No PACKAGE_NAME supplied."
+    exit 1
+fi
+
 GOLANG_VERSION=1.11
-PACKAGE_NAME=gitlab.com/monetha/protocol-go-sdk
 PACKAGE_FULL_PATH=/go/src/$PACKAGE_NAME
 VERSION=$(date +"%Y.%m.%d").$CI_PIPELINE_ID
-
-echo CI_PROJECT_PATH $CI_PROJECT_PATH
-echo CI_REPOSITORY_URL $CI_REPOSITORY_URL
-ECHO CI_PROJECT_URL $CI_PROJECT_URL
 
 docker run -i --rm \
 -v "$PWD":$PACKAGE_FULL_PATH \
