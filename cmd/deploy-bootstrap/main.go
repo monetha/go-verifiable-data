a46e9b69e3784f2b3523bc7c6232474aead58764
Dmitrij Koniajev <dimchansky@gmail.com>
2018-09-24 14:31:44 +0300

    reusing cmdutils in deploy-bootstrap

--- cmd/deploy-bootstrap/main.go
+++ cmd/deploy-bootstrap/main.go
@@ -1,29 +1,22 @@
 package main
 
 import (
-	"context"
 	"crypto/ecdsa"
-	"errors"
 	"flag"
-	"fmt"
 	"math/big"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
 	"github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/contracts/chequebook"
 	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/monetha/go-ethereum/backend"
+	"gitlab.com/monetha/protocol-go-sdk/cmd/cmdutils"
 	"gitlab.com/monetha/protocol-go-sdk/contracts"
 )
 
@@ -65,7 +58,7 @@ func main() {
 	}
 
 	ownerAuth := bind.NewKeyedTransactor(ownerKey)
-	ownerAddress := keyAddress(ownerKey, "invalid owner key")
+	ownerAddress := cmdutils.KeyAddress(ownerKey, "invalid owner key")
 	log.Warn("Loaded configuration", "owner_address", ownerAddress.Hex(), "backend_url", *backendURL)
 
 	var contractBackend chequebook.Backend
@@ -86,13 +79,13 @@ func main() {
 
 	contractBackend = backend.NewHandleNonceBackend(contractBackend, []common.Address{ownerAddress})
 
-	ctx := createCtrlCContext()
+	ctx := cmdutils.CreateCtrlCContext()
 
 	///////////////////////////////////////////////////////
 	// Check budgets
 	///////////////////////////////////////////////////////
 
-	checkBalance(ctx, contractBackend, ownerAddress, oneEthInWei)
+	cmdutils.CheckBalance(ctx, contractBackend, ownerAddress, oneEthInWei)
 
 	///////////////////////////////////////////////////////
 	// PassportLogic
@@ -100,8 +93,8 @@ func main() {
 
 	log.Warn("Deploying PassportLogic", "owner_address", ownerAddress)
 	passportLogicAddress, tx, passportLogicContract, err := contracts.DeployPassportLogicContract(ownerAuth, contractBackend)
-	checkErr(err, "deployment PassportLogic contract")
-	checkTx(ctx, contractBackend, tx.Hash())
+	cmdutils.CheckErr(err, "deployment PassportLogic contract")
+	cmdutils.CheckTx(ctx, contractBackend, tx.Hash())
 
 	log.Warn("PassportLogic deployed", "contract_address", passportLogicAddress.Hex())
 	_ = passportLogicContract
@@ -113,8 +106,8 @@ func main() {
 	version := "0.1"
 	log.Warn("Deploying PassportLogicRegistry", "owner_address", ownerAddress, "impl_version", version, "impl_address", passportLogicAddress)
 	passportLogicRegistryAddress, tx, passportLogicRegistryContract, err := contracts.DeployPassportLogicRegistryContract(ownerAuth, contractBackend, version, passportLogicAddress)
-	checkErr(err, "deployment PassportLogicRegistry contract")
-	checkTx(ctx, contractBackend, tx.Hash())
+	cmdutils.CheckErr(err, "deployment PassportLogicRegistry contract")
+	cmdutils.CheckTx(ctx, contractBackend, tx.Hash())
 
 	log.Warn("PassportLogicRegistry deployed", "contract_address", passportLogicRegistryAddress.Hex())
 	_ = passportLogicRegistryContract
@@ -125,106 +118,11 @@ func main() {
 
 	log.Warn("Deploying PassportFactory", "owner_address", ownerAddress, "registry", passportLogicRegistryAddress)
 	passportFactoryAddress, tx, passportFactoryContract, err := contracts.DeployPassportFactoryContract(ownerAuth, contractBackend, passportLogicRegistryAddress)
-	checkErr(err, "deployment PassportFactory contract")
-	checkTx(ctx, contractBackend, tx.Hash())
+	cmdutils.CheckErr(err, "deployment PassportFactory contract")
+	cmdutils.CheckTx(ctx, contractBackend, tx.Hash())
 
 	log.Warn("PassportFactory deployed", "contract_address", passportFactoryAddress.Hex())
 	_ = passportFactoryContract
 
 	log.Warn("Done.")
-}
-
-func checkErr(err error, hint string) {
-	if err != nil {
-		log.Error(hint, "err", err)
-		os.Exit(1)
-	}
-}
-
-func checkBalance(ctx context.Context, backend chequebook.Backend, address common.Address, minWei *big.Int) {
-	log.Warn("Checking balance", "address", address.Hex())
-
-	balance, err := backend.BalanceAt(ctx, address, nil)
-	hint := fmt.Sprintf("BalanceAt(%v)", address.Hex())
-	checkErr(err, hint)
-
-	if balance.Cmp(minWei) == -1 {
-		checkErr(fmt.Errorf("balance too low: %v wei < %v wei", balance, minWei), hint)
-	}
-}
-
-func checkTx(ctx context.Context, backend chequebook.Backend, txHash common.Hash) {
-	err := waitForTx(ctx, backend, txHash)
-	checkErr(err, fmt.Sprintf("tx 0x%x", txHash))
-}
-
-func waitForTx(ctx context.Context, backend chequebook.Backend, txHash common.Hash) error {
-	log.Warn("Waiting for transaction", "hash", txHash.Hex())
-
-	type commiter interface {
-		Commit()
-	}
-	if sim, ok := backend.(commiter); ok {
-		sim.Commit()
-		tr, err := backend.TransactionReceipt(ctx, txHash)
-		if err != nil {
-			return err
-		}
-		if tr.Status != types.ReceiptStatusSuccessful {
-			return fmt.Errorf("tx failed: %+v", tr)
-		}
-		return nil
-	}
-
-	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case <-time.After(4 * time.Second):
-		}
-
-		tr, err := backend.TransactionReceipt(ctx, txHash)
-		if err != nil {
-			if err == ethereum.NotFound {
-				continue
-			} else {
-				return err
-			}
-		} else {
-			if tr.Status != types.ReceiptStatusSuccessful {
-				return fmt.Errorf("tx failed: %+v", tr)
-			}
-			return nil
-		}
-	}
-}
-
-func keyAddress(privateKeyECDSA *ecdsa.PrivateKey, errorMsg string) common.Address {
-	addr, err := pubkeyToAddress(privateKeyECDSA.PublicKey)
-	if err != nil {
-		utils.Fatalf(errorMsg, err)
-	}
-	return addr
-}
-
-func pubkeyToAddress(p ecdsa.PublicKey) (common.Address, error) {
-	pubBytes := crypto.FromECDSAPub(&p)
-	if pubBytes == nil {
-		return common.Address{}, errors.New("invalid key")
-	}
-	return common.Address(common.BytesToAddress(crypto.Keccak256(pubBytes[1:])[12:])), nil
-}
-
-func createCtrlCContext() context.Context {
-	ctx, cancel := context.WithCancel(context.Background())
-	go func() {
-		defer cancel()
-
-		sigChan := make(chan os.Signal, 1)
-		signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT)
-		<-sigChan
-		log.Warn("got interrupt signal")
-	}()
-
-	return ctx
 }
