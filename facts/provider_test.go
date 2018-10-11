05f7bac03806c15efbe147ecd9ed777f94d3eab0
Dmitrij Koniajev <dimchansky@gmail.com>
2018-10-11 13:47:09 +0300

    refactoring facts package

---
+++ facts/provider_test.go
@@ -0,0 +1,64 @@
+package facts_test
+
+import (
+	"context"
+	"math/big"
+	"testing"
+
+	"gitlab.com/monetha/protocol-go-sdk/facts"
+)
+
+func TestFactProvider_WriteUint(t *testing.T) {
+	tests := []struct {
+		name string
+		key  [32]byte
+		data *big.Int
+		err  error
+	}{
+		{"0", [32]byte{9, 8, 7, 6}, bigInt(0), nil},
+		{"2^256-1", [32]byte{9, 8, 7, 6}, sub(exp(bigInt(2), bigInt(256)), bigInt(1)), nil},
+		{"-1 should fail", [32]byte{9, 8, 7, 6}, bigInt(-1), facts.ErrOutOfUint256Range},
+		{"2^256 should fail", [32]byte{9, 8, 7, 6}, exp(bigInt(2), bigInt(256)), facts.ErrOutOfUint256Range},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			passportAddress, factProviderSession := createPassportAndFactProviderSession(ctx, t)
+
+			err := facts.NewProvider(factProviderSession).WriteUint(ctx, passportAddress, tt.key, tt.data)
+			if err != tt.err {
+				t.Errorf("FactProvider.WriteUint() error = %v, want err = %v", err, tt.err)
+			}
+		})
+	}
+}
+
+func TestFactProvider_WriteInt(t *testing.T) {
+	tests := []struct {
+		name string
+		key  [32]byte
+		data *big.Int
+		err  error
+	}{
+		{"0", [32]byte{9, 8, 7, 6}, bigInt(0), nil},
+		{"1", [32]byte{9, 8, 7, 6}, bigInt(1), nil},
+		{"-1", [32]byte{9, 8, 7, 6}, bigInt(-1), nil},
+		{"2^255-1", [32]byte{9, 8, 7, 6}, sub(exp(bigInt(2), bigInt(255)), bigInt(1)), nil},
+		{"-2^255", [32]byte{9, 8, 7, 6}, neg(exp(bigInt(2), bigInt(255))), nil},
+		{"2^255 should fail", [32]byte{9, 8, 7, 6}, exp(bigInt(2), bigInt(255)), facts.ErrOutOfInt256Range},
+		{"-2^255-1 should fail", [32]byte{9, 8, 7, 6}, sub(neg(exp(bigInt(2), bigInt(255))), bigInt(1)), facts.ErrOutOfInt256Range},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			passportAddress, factProviderSession := createPassportAndFactProviderSession(ctx, t)
+
+			err := facts.NewProvider(factProviderSession).WriteInt(ctx, passportAddress, tt.key, tt.data)
+			if err != tt.err {
+				t.Errorf("FactProvider.WriteInt() error = %v, want err = %v", err, tt.err)
+			}
+		})
+	}
+}
--- facts/reader.go
+++ facts/reader.go
@@ -2,7 +2,6 @@ package facts
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"math/big"
 
@@ -16,11 +15,6 @@ import (
 
 var (
 	passportLogicInputParser *txdata.PassportLogicInputParser
-
-	// ErrOutOfUint256Range error returned when value is out of uint256 range [0; 2^256-1]
-	ErrOutOfUint256Range = errors.New("facts: out of uint256 range")
-	// ErrOutOfInt256Range error returned when value is out of int256 range [-2^255; 2^255-1]
-	ErrOutOfInt256Range = errors.New("facts: out of int256 range")
 )
 
 func init() {
@@ -31,192 +25,12 @@ func init() {
 	}
 }
 
-// FactProvider provides the facts
-type FactProvider eth.Session
-
 // FactReader reads the facts
 type FactReader eth.Eth
-
-// NewProvider converts session to FactProvider
-func NewProvider(s *eth.Session) *FactProvider {
-	return (*FactProvider)(s)
-}
 
 // NewReader converts eth to FactReader
 func NewReader(e *eth.Eth) *FactReader {
 	return (*FactReader)(e)
-}
-
-// WriteTxData writes data for the specific key (uses transaction data)
-func (p *FactProvider) WriteTxData(ctx context.Context, passportAddress common.Address, key [32]byte, data []byte) error {
-	backend := p.Backend
-	factProviderAuth := &p.TransactOpts
-
-	p.Log("Initialising passport", "passport", passportAddress)
-	passportLogicContract, err := contracts.NewPassportLogicContract(passportAddress, backend)
-	if err != nil {
-		return fmt.Errorf("facts: NewPassportLogicContract: %v", err)
-	}
-
-	p.Log("Writing tx data to passport", "fact_provider", factProviderAuth.From.Hex(), "key", key)
-	tx, err := passportLogicContract.SetTxDataBlockNumber(factProviderAuth, key, data)
-	if err != nil {
-		return fmt.Errorf("facts: SetTxDataBlockNumber: %v", err)
-	}
-	_, err = p.WaitForTxReceipt(ctx, tx.Hash())
-
-	return err
-}
-
-// WriteBytes writes data for the specific key (uses Ethereum storage)
-func (p *FactProvider) WriteBytes(ctx context.Context, passportAddress common.Address, key [32]byte, data []byte) error {
-	backend := p.Backend
-	factProviderAuth := &p.TransactOpts
-
-	p.Log("Initialising passport", "passport", passportAddress)
-	passportLogicContract, err := contracts.NewPassportLogicContract(passportAddress, backend)
-	if err != nil {
-		return fmt.Errorf("facts: NewPassportLogicContract: %v", err)
-	}
-
-	p.Log("Writing bytes to passport", "fact_provider", factProviderAuth.From.Hex(), "key", key)
-	tx, err := passportLogicContract.SetBytes(factProviderAuth, key, data)
-	if err != nil {
-		return fmt.Errorf("facts: SetBytes: %v", err)
-	}
-	_, err = p.WaitForTxReceipt(ctx, tx.Hash())
-
-	return err
-}
-
-// WriteString writes data for the specific key (uses Ethereum storage)
-func (p *FactProvider) WriteString(ctx context.Context, passportAddress common.Address, key [32]byte, data string) error {
-	backend := p.Backend
-	factProviderAuth := &p.TransactOpts
-
-	p.Log("Initialising passport", "passport", passportAddress)
-	passportLogicContract, err := contracts.NewPassportLogicContract(passportAddress, backend)
-	if err != nil {
-		return fmt.Errorf("facts: NewPassportLogicContract: %v", err)
-	}
-
-	p.Log("Writing string to passport", "fact_provider", factProviderAuth.From.Hex(), "key", key)
-	tx, err := passportLogicContract.SetString(factProviderAuth, key, data)
-	if err != nil {
-		return fmt.Errorf("facts: SetString: %v", err)
-	}
-	_, err = p.WaitForTxReceipt(ctx, tx.Hash())
-
-	return err
-}
-
-// WriteAddress writes data for the specific key (uses Ethereum storage)
-func (p *FactProvider) WriteAddress(ctx context.Context, passportAddress common.Address, key [32]byte, data common.Address) error {
-	backend := p.Backend
-	factProviderAuth := &p.TransactOpts
-
-	p.Log("Initialising passport", "passport", passportAddress)
-	passportLogicContract, err := contracts.NewPassportLogicContract(passportAddress, backend)
-	if err != nil {
-		return fmt.Errorf("facts: NewPassportLogicContract: %v", err)
-	}
-
-	p.Log("Writing address to passport", "fact_provider", factProviderAuth.From.Hex(), "key", key)
-	tx, err := passportLogicContract.SetAddress(factProviderAuth, key, data)
-	if err != nil {
-		return fmt.Errorf("facts: SetAddress: %v", err)
-	}
-	_, err = p.WaitForTxReceipt(ctx, tx.Hash())
-
-	return err
-}
-
-// WriteUint writes data for the specific key (uses Ethereum storage)
-func (p *FactProvider) WriteUint(ctx context.Context, passportAddress common.Address, key [32]byte, data *big.Int) error {
-	if data == nil {
-		panic("big-integer cannot be nil")
-	}
-
-	if data.Sign() == -1 || data.BitLen() > 256 {
-		return ErrOutOfUint256Range
-	}
-
-	data = new(big.Int).Set(data)
-
-	backend := p.Backend
-	factProviderAuth := &p.TransactOpts
-
-	p.Log("Initialising passport", "passport", passportAddress)
-	passportLogicContract, err := contracts.NewPassportLogicContract(passportAddress, backend)
-	if err != nil {
-		return fmt.Errorf("facts: NewPassportLogicContract: %v", err)
-	}
-
-	p.Log("Writing uint to passport", "fact_provider", factProviderAuth.From.Hex(), "key", key)
-	tx, err := passportLogicContract.SetUint(factProviderAuth, key, data)
-	if err != nil {
-		return fmt.Errorf("facts: SetUint: %v", err)
-	}
-	_, err = p.WaitForTxReceipt(ctx, tx.Hash())
-
-	return err
-}
-
-var (
-	two255    = new(big.Int).Exp(big.NewInt(2), big.NewInt(255), nil) // = 2^255
-	maxInt256 = new(big.Int).Sub(two255, big.NewInt(1))               // = 2^255-1
-	minInt256 = new(big.Int).Neg(two255)                              // = -2^255
-)
-
-// WriteInt writes data for the specific key (uses Ethereum storage)
-func (p *FactProvider) WriteInt(ctx context.Context, passportAddress common.Address, key [32]byte, data *big.Int) error {
-	if data == nil {
-		panic("big-integer cannot be nil")
-	}
-	if data.Cmp(maxInt256) == 1 || minInt256.Cmp(data) == 1 {
-		return ErrOutOfInt256Range
-	}
-
-	data = new(big.Int).Set(data)
-
-	backend := p.Backend
-	factProviderAuth := &p.TransactOpts
-
-	p.Log("Initialising passport", "passport", passportAddress)
-	passportLogicContract, err := contracts.NewPassportLogicContract(passportAddress, backend)
-	if err != nil {
-		return fmt.Errorf("facts: NewPassportLogicContract: %v", err)
-	}
-
-	p.Log("Writing int to passport", "fact_provider", factProviderAuth.From.Hex(), "key", key)
-	tx, err := passportLogicContract.SetInt(factProviderAuth, key, data)
-	if err != nil {
-		return fmt.Errorf("facts: SetInt: %v", err)
-	}
-	_, err = p.WaitForTxReceipt(ctx, tx.Hash())
-
-	return err
-}
-
-// WriteBool writes data for the specific key (uses Ethereum storage)
-func (p *FactProvider) WriteBool(ctx context.Context, passportAddress common.Address, key [32]byte, data bool) error {
-	backend := p.Backend
-	factProviderAuth := &p.TransactOpts
-
-	p.Log("Initialising passport", "passport", passportAddress)
-	passportLogicContract, err := contracts.NewPassportLogicContract(passportAddress, backend)
-	if err != nil {
-		return fmt.Errorf("facts: NewPassportLogicContract: %v", err)
-	}
-
-	p.Log("Writing bool to passport", "fact_provider", factProviderAuth.From.Hex(), "key", key)
-	tx, err := passportLogicContract.SetBool(factProviderAuth, key, data)
-	if err != nil {
-		return fmt.Errorf("facts: SetBool: %v", err)
-	}
-	_, err = p.WaitForTxReceipt(ctx, tx.Hash())
-
-	return err
 }
 
 // ReadTxData reads the data from the specific key of the given data provider.
