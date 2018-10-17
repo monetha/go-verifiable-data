00bf4708d98233645ed4a56a1271dc0af407ea6d
Dmitrij Koniajev <dimchansky@gmail.com>
2018-10-17 14:36:52 +0300

    more tests added: filter all updates order, filter all deletes order

--- facts/historian_test.go
+++ facts/historian_test.go
@@ -23,66 +23,66 @@ func TestHistorian_FilterChanges(t *testing.T) {
 
 	key := [32]byte{99, 88, 77, 66, 55, 44, 33, 22, 11}
 
-	var expectedChanges []changeDetails
+	var allChanges []changeDetails
 	// make some updates
 	if err := provider.WriteTxData(ctx, passportAddress, key, []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}); err != nil {
 		t.Errorf("Provider.WriteTxData() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Updated, DataType: data.TxData, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Updated, DataType: data.TxData, FactProvider: factProviderAddress, Key: key})
 	if err := provider.WriteBytes(ctx, passportAddress, key, []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}); err != nil {
 		t.Errorf("Provider.WriteBytes() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Updated, DataType: data.Bytes, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Updated, DataType: data.Bytes, FactProvider: factProviderAddress, Key: key})
 	if err := provider.WriteString(ctx, passportAddress, key, "test only string"); err != nil {
 		t.Errorf("Provider.WriteString() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Updated, DataType: data.String, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Updated, DataType: data.String, FactProvider: factProviderAddress, Key: key})
 	if err := provider.WriteAddress(ctx, passportAddress, key, common.HexToAddress("0xaF4DcE16Da2877f8c9e00544c93B62Ac40631F16")); err != nil {
 		t.Errorf("Provider.WriteAddress() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Updated, DataType: data.Address, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Updated, DataType: data.Address, FactProvider: factProviderAddress, Key: key})
 	if err := provider.WriteInt(ctx, passportAddress, key, big.NewInt(-123456)); err != nil {
 		t.Errorf("Provider.WriteInt() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Updated, DataType: data.Int, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Updated, DataType: data.Int, FactProvider: factProviderAddress, Key: key})
 	if err := provider.WriteUint(ctx, passportAddress, key, big.NewInt(123456)); err != nil {
 		t.Errorf("Provider.WriteUint() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Updated, DataType: data.Uint, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Updated, DataType: data.Uint, FactProvider: factProviderAddress, Key: key})
 	if err := provider.WriteBool(ctx, passportAddress, key, true); err != nil {
 		t.Errorf("Provider.WriteBool() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Updated, DataType: data.Bool, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Updated, DataType: data.Bool, FactProvider: factProviderAddress, Key: key})
 
 	// make some deletes
 	if err := provider.DeleteTxData(ctx, passportAddress, key); err != nil {
 		t.Errorf("Provider.DeleteTxData() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Deleted, DataType: data.TxData, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Deleted, DataType: data.TxData, FactProvider: factProviderAddress, Key: key})
 	if err := provider.DeleteBytes(ctx, passportAddress, key); err != nil {
 		t.Errorf("Provider.DeleteBytes() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Deleted, DataType: data.Bytes, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Deleted, DataType: data.Bytes, FactProvider: factProviderAddress, Key: key})
 	if err := provider.DeleteString(ctx, passportAddress, key); err != nil {
 		t.Errorf("Provider.DeleteString() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Deleted, DataType: data.String, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Deleted, DataType: data.String, FactProvider: factProviderAddress, Key: key})
 	if err := provider.DeleteAddress(ctx, passportAddress, key); err != nil {
 		t.Errorf("Provider.DeleteAddress() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Deleted, DataType: data.Address, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Deleted, DataType: data.Address, FactProvider: factProviderAddress, Key: key})
 	if err := provider.DeleteInt(ctx, passportAddress, key); err != nil {
 		t.Errorf("Provider.DeleteInt() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Deleted, DataType: data.Int, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Deleted, DataType: data.Int, FactProvider: factProviderAddress, Key: key})
 	if err := provider.DeleteUint(ctx, passportAddress, key); err != nil {
 		t.Errorf("Provider.DeleteUint() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Deleted, DataType: data.Uint, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Deleted, DataType: data.Uint, FactProvider: factProviderAddress, Key: key})
 	if err := provider.DeleteBool(ctx, passportAddress, key); err != nil {
 		t.Errorf("Provider.DeleteBool() error = %v", err)
 	}
-	expectedChanges = append(expectedChanges, changeDetails{ChangeType: change.Deleted, DataType: data.Bool, FactProvider: factProviderAddress, Key: key})
+	allChanges = append(allChanges, changeDetails{ChangeType: change.Deleted, DataType: data.Bool, FactProvider: factProviderAddress, Key: key})
 
 	eth := factProviderSession.Eth
 	hist := facts.NewHistorian(eth)
@@ -112,7 +112,15 @@ func TestHistorian_FilterChanges(t *testing.T) {
 	}
 
 	t.Run("filter all order", func(t *testing.T) {
-		testFilterChangesAll(t, hist, passportAddress, expectedChanges)
+		testFilterChangesAll(t, hist, passportAddress, allChanges)
+	})
+
+	t.Run("filter all updates order", func(t *testing.T) {
+		testFilterChangesAllUpdates(t, hist, passportAddress, allChanges)
+	})
+
+	t.Run("filter all deletes order", func(t *testing.T) {
+		testFilterChangesAllDeletes(t, hist, passportAddress, allChanges)
 	})
 }
 
@@ -138,7 +146,7 @@ func testFilterChangesExactOne(t *testing.T, hist *facts.Historian, changeType c
 
 	changes, err := it.ToSlice()
 	if err != nil {
-		t.Errorf("ChnageIterator.ToSlice() error = %v", err)
+		t.Errorf("ChangeIterator.ToSlice() error = %v", err)
 	}
 
 	if len(changes) != 1 {
@@ -166,7 +174,7 @@ func testFilterChangesExactOne(t *testing.T, hist *facts.Historian, changeType c
 	}
 }
 
-func testFilterChangesAll(t *testing.T, hist *facts.Historian, passportAddress common.Address, expectedChanges []changeDetails) {
+func testFilterChangesAll(t *testing.T, hist *facts.Historian, passportAddress common.Address, allChanges []changeDetails) {
 	it, err := hist.FilterChanges(nil, passportAddress)
 	if err != nil {
 		t.Errorf("Historian.FilterChanges() error = %v", err)
@@ -174,18 +182,87 @@ func testFilterChangesAll(t *testing.T, hist *facts.Historian, passportAddress c
 
 	changes, err := it.ToSlice()
 	if err != nil {
-		t.Errorf("ChnageIterator.ToSlice() error = %v", err)
+		t.Errorf("ChangeIterator.ToSlice() error = %v", err)
 	}
 
 	var actualChanges []changeDetails
 	for _, ch := range changes {
-		actual := changeDetails{
+		actualChanges = append(actualChanges, changeDetails{
 			ChangeType:   ch.ChangeType,
 			DataType:     ch.DataType,
 			FactProvider: ch.FactProvider,
 			Key:          ch.Key,
+		})
+	}
+
+	if diff := deep.Equal(allChanges, actualChanges); diff != nil {
+		t.Error(diff)
+	}
+}
+
+func testFilterChangesAllUpdates(t *testing.T, hist *facts.Historian, passportAddress common.Address, allChanges []changeDetails) {
+	opts := &facts.ChangesFilterOpts{ChangeType: []change.Type{change.Updated}}
+	it, err := hist.FilterChanges(opts, passportAddress)
+	if err != nil {
+		t.Errorf("Historian.FilterChanges() error = %v", err)
+	}
+
+	changes, err := it.ToSlice()
+	if err != nil {
+		t.Errorf("ChangeIterator.ToSlice() error = %v", err)
+	}
+
+	var actualChanges []changeDetails
+	for _, ch := range changes {
+		actualChanges = append(actualChanges, changeDetails{
+			ChangeType:   ch.ChangeType,
+			DataType:     ch.DataType,
+			FactProvider: ch.FactProvider,
+			Key:          ch.Key,
+		})
+	}
+
+	var expectedChanges []changeDetails
+	for _, ch := range allChanges {
+		if ch.ChangeType != change.Updated {
+			continue
 		}
-		actualChanges = append(actualChanges, actual)
+		expectedChanges = append(expectedChanges, ch)
+	}
+
+	if diff := deep.Equal(expectedChanges, actualChanges); diff != nil {
+		t.Error(diff)
+	}
+}
+
+func testFilterChangesAllDeletes(t *testing.T, hist *facts.Historian, passportAddress common.Address, allChanges []changeDetails) {
+	opts := &facts.ChangesFilterOpts{ChangeType: []change.Type{change.Deleted}}
+	it, err := hist.FilterChanges(opts, passportAddress)
+	if err != nil {
+		t.Errorf("Historian.FilterChanges() error = %v", err)
+	}
+
+	changes, err := it.ToSlice()
+	if err != nil {
+		t.Errorf("ChangeIterator.ToSlice() error = %v", err)
+	}
+
+	var actualChanges []changeDetails
+	for _, ch := range changes {
+		actualChanges = append(actualChanges, changeDetails{
+			ChangeType:   ch.ChangeType,
+			DataType:     ch.DataType,
+			FactProvider: ch.FactProvider,
+			Key:          ch.Key,
+		})
+	}
+
+	var expectedChanges []changeDetails
+	for _, ch := range allChanges {
+		if ch.ChangeType != change.Deleted {
+			continue
+		}
+		expectedChanges = append(expectedChanges, ch)
 	}
 
 	if diff := deep.Equal(expectedChanges, actualChanges); diff != nil {
