aa9007b52480469b763ca8007fca7c62525b1fed
Dmitrij Koniajev <dimchansky@gmail.com>
2019-03-14 15:33:38 +0200

    TestIPFS_Cat test simplified using recorder

--- ipfs/ipfs_test.go
+++ ipfs/ipfs_test.go
@@ -1,39 +1,29 @@
 package ipfs_test
 
 import (
-	"bytes"
 	"context"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"strings"
 	"testing"
 
 	"github.com/cheekybits/is"
+	"github.com/dnaeon/go-vcr/recorder"
 	"github.com/monetha/reputation-go-sdk/ipfs"
 )
 
 func TestIPFS_Cat(t *testing.T) {
+	// start http recorder
+	r, err := recorder.New("fixtures/hello-world")
+	if err != nil {
+		panic(err)
+	}
+	defer r.Stop() // Make sure recorder is stopped once done with it
+
 	is := is.New(t)
 	expectedStr := "hello world!"
 
-	fs := ipfs.NewWithClient("https://ipfs.infura.io:5001",
-		newTestClient(func(req *http.Request) *http.Response {
-			defer cleanUpRequest(req)
-
-			if req.Method != "POST" {
-				return httpResponse(http.StatusMethodNotAllowed, `method not allowed`)
-			}
-
-			switch req.URL.String() {
-			case "https://ipfs.infura.io:5001/api/v0/add?pin=true&progress=false":
-				return httpResponse(http.StatusOK, `{"Name":"QmTp2hEo8eXRp6wg7jXv1BLCMh5a4F3B7buAUZNZUu772j","Hash":"QmTp2hEo8eXRp6wg7jXv1BLCMh5a4F3B7buAUZNZUu772j","Size":"20"}`)
-			case "https://ipfs.infura.io:5001/api/v0/cat?arg=QmTp2hEo8eXRp6wg7jXv1BLCMh5a4F3B7buAUZNZUu772j":
-				return httpResponse(http.StatusOK, expectedStr)
-			default:
-				return httpResponse(http.StatusNotFound, `not found`)
-			}
-		}))
+	fs := ipfs.NewWithClient("https://ipfs.infura.io:5001", &http.Client{Transport: r})
 
 	ctx := context.TODO()
 	addResult, err := fs.Add(ctx, strings.NewReader(expectedStr))
@@ -55,15 +45,6 @@ func TestIPFS_Cat(t *testing.T) {
 	is.Equal(expectedStr, actualStr)
 }
 
-func cleanUpRequest(req *http.Request) {
-	if req != nil {
-		if rb := req.Body; rb != nil {
-			_, _ = io.Copy(ioutil.Discard, rb)
-			_ = rb.Close()
-		}
-	}
-}
-
 func copyToString(r io.Reader) (res string, err error) {
 	if c, ok := r.(io.Closer); ok {
 		defer c.Close()
@@ -74,24 +55,4 @@ func copyToString(r io.Reader) (res string, err error) {
 		res = sb.String()
 	}
 	return
-}
-
-func httpResponse(statusCode int, body string) *http.Response {
-	return &http.Response{
-		StatusCode: statusCode,
-		Body:       ioutil.NopCloser(bytes.NewBufferString(body)),
-		Header:     make(http.Header),
-	}
-}
-
-type roundTripFunc func(req *http.Request) *http.Response
-
-func (f roundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
-	return f(req), nil
-}
-
-func newTestClient(fn roundTripFunc) *http.Client {
-	return &http.Client{
-		Transport: roundTripFunc(fn),
-	}
 }
