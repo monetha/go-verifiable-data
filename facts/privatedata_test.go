85ebd3699110ed2b72d68e5c35b073292fa318bc
Dmitrij Koniajev <dimchansky@gmail.com>
2019-04-26 14:43:54 +0300

    refactoring private data tests

--- facts/privatedata_test.go
+++ facts/privatedata_test.go
@@ -11,168 +11,113 @@ import (
 	"github.com/monetha/reputation-go-sdk/facts"
 )
 
-func TestNewPrivateData_ReadPrivateData(t *testing.T) {
-	// start http recorder
-	r, err := recorder.New("fixtures/private-data-write-read")
-	if err != nil {
-		panic(err)
-	}
-	defer func() { _ = r.Stop() }() // Make sure recorder is stopped once done with it
-	c := &http.Client{Transport: r}
+func TestPrivateData(t *testing.T) {
+	arrangeActAssert := func(actsAssert func(
+		ctx context.Context,
+		rd *facts.PrivateDataReader,
+		pa *passportWithActors,
+		wpdRes *facts.WritePrivateDataResult,
+	)) {
+		// start http recorder
+		r, err := recorder.New("fixtures/private-data-write-read")
+		if err != nil {
+			panic(err)
+		}
+		defer func() { _ = r.Stop() }() // Make sure recorder is stopped once done with it
+		c := &http.Client{Transport: r}
 
-	ctx := context.Background()
-	rnd := rand.New(rand.NewSource(randSeed))
+		ctx := context.Background()
+		rnd := rand.New(rand.NewSource(randSeed))
 
-	pa := newPassportWithActors(ctx, t, rnd)
+		pa := newPassportWithActors(ctx, t, rnd)
 
-	passportAddress := pa.PassportAddress
-	factProviderAddress := pa.FactProviderAddress
+		wr, err := facts.NewPrivateDataWriterWithClient(pa.FactProviderSession(), ipfsURL, c)
+		if err != nil {
+			t.Fatalf("facts.NewPrivateDataWriterWithClient: %v", err)
+		}
 
-	wr, err := facts.NewPrivateDataWriterWithClient(pa.FactProviderSession(), ipfsURL, c)
-	if err != nil {
-		t.Fatalf("facts.NewPrivateDataWriterWithClient: %v", err)
-	}
+		rd, err := facts.NewPrivateDataReaderWithClient(pa.Eth, ipfsURL, c)
+		if err != nil {
+			t.Fatalf("facts.NewPrivateDataReaderWithClient: %v", err)
+		}
 
-	rd, err := facts.NewPrivateDataReaderWithClient(pa.Eth, ipfsURL, c)
-	if err != nil {
-		t.Fatalf("facts.NewPrivateDataReaderWithClient: %v", err)
-	}
+		wpdRes, err := wr.WritePrivateData(ctx, pa.PassportAddress, factKey, factData, rnd)
+		if err != nil {
+			t.Fatalf("WritePrivateData: %v", err)
+		}
 
-	_, err = wr.WritePrivateData(ctx, passportAddress, factKey, factData, rnd)
-	if err != nil {
-		t.Fatalf("WritePrivateData: %v", err)
-	}
-
-	decryptedData, err := rd.ReadPrivateData(ctx, pa.PassportOwnerKey, passportAddress, factProviderAddress, factKey)
-	if err != nil {
-		t.Fatalf("ReadPrivateData: %v", err)
-	}
-
-	if bytes.Compare(factData, decryptedData) != 0 {
-		t.Errorf("wanted data %v, but got %v", factData, decryptedData)
-	}
-}
-
-func TestNewPrivateData_ReadSecretKey(t *testing.T) {
-	// start http recorder
-	r, err := recorder.New("fixtures/private-data-write-read")
-	if err != nil {
-		panic(err)
-	}
-	defer func() { _ = r.Stop() }() // Make sure recorder is stopped once done with it
-	c := &http.Client{Transport: r}
-
-	ctx := context.Background()
-	rnd := rand.New(rand.NewSource(randSeed))
-
-	pa := newPassportWithActors(ctx, t, rnd)
-
-	passportAddress := pa.PassportAddress
-	factProviderAddress := pa.FactProviderAddress
-
-	wr, err := facts.NewPrivateDataWriterWithClient(pa.FactProviderSession(), ipfsURL, c)
-	if err != nil {
-		t.Fatalf("facts.NewPrivateDataWriterWithClient: %v", err)
+		actsAssert(ctx, rd, pa, wpdRes)
 	}
 
-	rd, err := facts.NewPrivateDataReaderWithClient(pa.Eth, ipfsURL, c)
-	if err != nil {
-		t.Fatalf("facts.NewPrivateDataReaderWithClient: %v", err)
-	}
+	t.Run("ReadPrivateData", func(t *testing.T) {
+		arrangeActAssert(func(
+			ctx context.Context,
+			rd *facts.PrivateDataReader,
+			pa *passportWithActors,
+			wpdRes *facts.WritePrivateDataResult,
+		) {
+			decryptedData, err := rd.ReadPrivateData(ctx, pa.PassportOwnerKey, pa.PassportAddress, pa.FactProviderAddress, factKey)
+			if err != nil {
+				t.Fatalf("ReadPrivateData: %v", err)
+			}
 
-	wpdRes, err := wr.WritePrivateData(ctx, passportAddress, factKey, factData, rnd)
-	if err != nil {
-		t.Fatalf("WritePrivateData: %v", err)
-	}
+			if bytes.Compare(factData, decryptedData) != 0 {
+				t.Errorf("wanted data %v, but got %v", factData, decryptedData)
+			}
+		})
+	})
 
-	secretKey, err := rd.ReadSecretKey(ctx, pa.PassportOwnerKey, passportAddress, factProviderAddress, factKey)
-	if err != nil {
-		t.Fatalf("ReadPrivateData: %v", err)
-	}
+	t.Run("ReadSecretKey", func(t *testing.T) {
+		arrangeActAssert(func(
+			ctx context.Context,
+			rd *facts.PrivateDataReader,
+			pa *passportWithActors,
+			wpdRes *facts.WritePrivateDataResult,
+		) {
+			secretKey, err := rd.ReadSecretKey(ctx, pa.PassportOwnerKey, pa.PassportAddress, pa.FactProviderAddress, factKey)
+			if err != nil {
+				t.Fatalf("ReadPrivateData: %v", err)
+			}
 
-	if bytes.Compare(wpdRes.DataKey, secretKey) != 0 {
-		t.Errorf("wanted secret key %v, but got %v", wpdRes.DataKey, secretKey)
-	}
-}
+			if bytes.Compare(wpdRes.DataKey, secretKey) != 0 {
+				t.Errorf("wanted secret key %v, but got %v", wpdRes.DataKey, secretKey)
+			}
+		})
+	})
 
-func TestNewPrivateData_DecryptPrivateData(t *testing.T) {
-	// start http recorder
-	r, err := recorder.New("fixtures/private-data-write-read")
-	if err != nil {
-		panic(err)
-	}
-	defer func() { _ = r.Stop() }() // Make sure recorder is stopped once done with it
-	c := &http.Client{Transport: r}
+	t.Run("DecryptPrivateData", func(t *testing.T) {
+		arrangeActAssert(func(
+			ctx context.Context,
+			rd *facts.PrivateDataReader,
+			pa *passportWithActors,
+			wpdRes *facts.WritePrivateDataResult,
+		) {
+			decryptedData, err := rd.DecryptPrivateData(ctx, wpdRes.DataIPFSHash, wpdRes.DataKey, nil)
+			if err != nil {
+				t.Fatalf("ReadPrivateData: %v", err)
+			}
 
-	ctx := context.Background()
-	rnd := rand.New(rand.NewSource(randSeed))
+			if bytes.Compare(factData, decryptedData) != 0 {
+				t.Errorf("wanted data %v, but got %v", factData, decryptedData)
+			}
+		})
+	})
 
-	pa := newPassportWithActors(ctx, t, rnd)
+	t.Run("ReadHistoryPrivateData", func(t *testing.T) {
+		arrangeActAssert(func(
+			ctx context.Context,
+			rd *facts.PrivateDataReader,
+			pa *passportWithActors,
+			wpdRes *facts.WritePrivateDataResult,
+		) {
+			decryptedData, err := rd.ReadHistoryPrivateData(ctx, pa.PassportOwnerKey, pa.PassportAddress, wpdRes.TransactionHash)
+			if err != nil {
+				t.Fatalf("ReadPrivateData: %v", err)
+			}
 
-	passportAddress := pa.PassportAddress
-
-	wr, err := facts.NewPrivateDataWriterWithClient(pa.FactProviderSession(), ipfsURL, c)
-	if err != nil {
-		t.Fatalf("facts.NewPrivateDataWriterWithClient: %v", err)
-	}
-
-	rd, err := facts.NewPrivateDataReaderWithClient(pa.Eth, ipfsURL, c)
-	if err != nil {
-		t.Fatalf("facts.NewPrivateDataReaderWithClient: %v", err)
-	}
-
-	wpdRes, err := wr.WritePrivateData(ctx, passportAddress, factKey, factData, rnd)
-	if err != nil {
-		t.Fatalf("WritePrivateData: %v", err)
-	}
-
-	decryptedData, err := rd.DecryptPrivateData(ctx, wpdRes.DataIPFSHash, wpdRes.DataKey, nil)
-	if err != nil {
-		t.Fatalf("ReadPrivateData: %v", err)
-	}
-
-	if bytes.Compare(factData, decryptedData) != 0 {
-		t.Errorf("wanted data %v, but got %v", factData, decryptedData)
-	}
-}
-
-func TestNewPrivateData_ReadHistoryPrivateData(t *testing.T) {
-	// start http recorder
-	r, err := recorder.New("fixtures/private-data-write-read")
-	if err != nil {
-		panic(err)
-	}
-	defer func() { _ = r.Stop() }() // Make sure recorder is stopped once done with it
-	c := &http.Client{Transport: r}
-
-	ctx := context.Background()
-	rnd := rand.New(rand.NewSource(randSeed))
-
-	pa := newPassportWithActors(ctx, t, rnd)
-
-	passportAddress := pa.PassportAddress
-
-	wr, err := facts.NewPrivateDataWriterWithClient(pa.FactProviderSession(), ipfsURL, c)
-	if err != nil {
-		t.Fatalf("facts.NewPrivateDataWriterWithClient: %v", err)
-	}
-
-	rd, err := facts.NewPrivateDataReaderWithClient(pa.Eth, ipfsURL, c)
-	if err != nil {
-		t.Fatalf("facts.NewPrivateDataReaderWithClient: %v", err)
-	}
-
-	wpdRes, err := wr.WritePrivateData(ctx, passportAddress, factKey, factData, rnd)
-	if err != nil {
-		t.Fatalf("WritePrivateData: %v", err)
-	}
-
-	decryptedData, err := rd.ReadHistoryPrivateData(ctx, pa.PassportOwnerKey, passportAddress, wpdRes.TransactionHash)
-	if err != nil {
-		t.Fatalf("ReadPrivateData: %v", err)
-	}
-
-	if bytes.Compare(factData, decryptedData) != 0 {
-		t.Errorf("wanted data %v, but got %v", factData, decryptedData)
-	}
+			if bytes.Compare(factData, decryptedData) != 0 {
+				t.Errorf("wanted data %v, but got %v", factData, decryptedData)
+			}
+		})
+	})
 }
