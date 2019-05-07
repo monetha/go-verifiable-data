61fb850498b165e89c410da4d1c85d251cfe7a9c
Dmitrij Koniajev <dimchansky@gmail.com>
2019-05-07 15:01:30 +0300

    added state validation check

--- facts/exchangefinisher.go
+++ facts/exchangefinisher.go
@@ -7,7 +7,13 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/monetha/reputation-go-sdk/contracts"
 	"github.com/monetha/reputation-go-sdk/eth"
+	"github.com/monetha/reputation-go-sdk/types/exchange"
 	"github.com/pkg/errors"
+)
+
+var (
+	// ErrExchangeMustBeAccepted returned when private data exchange is not in Accepted state
+	ErrExchangeMustBeAccepted = errors.New("private data exchange must be in Accepted state")
 )
 
 // ExchangeFinisher allows to finish data exchange
@@ -24,6 +30,20 @@ func (f *ExchangeFinisher) FinishPrivateDataExchange(ctx context.Context, passpo
 	backend := f.Backend
 
 	c := contracts.InitPassportLogicContract(passportAddress, backend)
+
+	ex, err := c.PrivateDataExchanges(nil, exchangeIdx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get proposed private data exchange")
+	}
+
+	// it should be proposed
+	exState := exchange.StateType(ex.State)
+	if exState != exchange.Accepted {
+		return ErrExchangeMustBeAccepted
+	}
+
+	// TODO: add expiration check
+
 	tx, err := c.FinishPrivateDataExchange(&f.TransactOpts, exchangeIdx)
 	if err != nil {
 		return errors.Wrap(err, "failed to finish accepted private data exchange")
